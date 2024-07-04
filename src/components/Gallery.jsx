7c5f9dc3b79951ba51f871f4bdb6707292d3dacf
MariFer14 <ferarenilla@gmail.com>
2024-07-04 15:14:14 -0500

    Agregando archivo gallery.css

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ContexAPI } from "../contex/ContexAPI";
 import HeartEmpty from "../assets/icons/heart.svg";
 import HeartFilled from "../assets/icons/heart-filled.svg";
+import "../assets/css/gallery.css";
 
 const Gallery = ({ photos }) => {
   const { toggleFavorito } = useContext(ContexAPI);
@@ -17,17 +18,30 @@ const Gallery = ({ photos }) => {
   return (
     <div className="gallery grid-columns-4 p-3">
       {photos.map((photo) => (
-        <div key={photo.id}>
-          <img
-            src={photo.src.original}
-            alt={photo.alt}
-            style={{ width: "400px", height: "400px" }}
-          />
+        <div key={photo.id} className="gallery-item">
+          <div className="image-container">
+            <img
+              src={photo.src.original}
+              alt={photo.alt}
+              style={{ width: "400px", height: "400px" }}
+              className="photo"
+            />
             {photo.isFavorite ? (
-              <img src={HeartFilled} alt="Corazón lleno" onClick={() => handleToggleFavorito(photo.id)} style={{ cursor: "pointer" }} />
+              <img
+                src={HeartFilled}
+                alt="Corazón lleno"
+                onClick={() => handleToggleFavorito(photo.id)}
+                className="iconHeart"
+              />
             ) : (
-              <img src={HeartEmpty}  alt="Corazón vacío" onClick={() => handleToggleFavorito(photo.id)} style={{ cursor: "pointer" }}/>
+              <img
+                src={HeartEmpty}
+                alt="Corazón vacío"
+                onClick={() => handleToggleFavorito(photo.id)}
+                className="iconHeart"
+              />
             )}
+          </div>
         </div>
       ))}
     </div>
--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,17 +1,23 @@
 import React, { useContext } from "react";
-import Gallery from "../components/Gallery";
 import { ContexAPI } from "../contex/ContexAPI";
 
 const Favorites = () => {
   const { favoritos } = useContext(ContexAPI)
-  
-  const fotosFavoritas = favoritos.filter(photo => photo.isFavorite);
+
 
   return (
     <div>
-      <h1>Fotos favoritas</h1>
+      <h1 style={{marginLeft:"50px", marginTop:"50px", color:"darkgreen"}}>Fotos favoritas</h1>
       <div className="p-3 gallery grid-columns-4">
-        <Gallery photos={fotosFavoritas} />
+        {favoritos.map((photo) => (
+          <div key={photo.id}> 
+             <img
+            src={photo.src.original}
+            alt={photo.alt}
+            style={{ width: "400px", height: "400px" }}
+          />
+          </div>
+        ))}
       </div>
     </div>
   );
